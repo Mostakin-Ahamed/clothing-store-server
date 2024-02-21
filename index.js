651f5523144c529e5c4b6d02f9f5b68f5ac986cc
Mostakin-Ahamed <mostakinahamedwork@gmail.com>
2024-02-22 02:47:23 +0600

    data fetched from server to site

--- index.js
+++ index.js
@@ -26,20 +26,30 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("ClothingStore").collection("itemsCollection");
+
+    app.get('/', async(req, res)=>{
+        const result = await database.find().toArray();
+        res.send(result)
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
   }
 }
 run().catch(console.dir);
 
 
-app.get('/', (req, res)=>{
-    res.send('clothing store server is running')
-})
+// app.get('/', (req, res)=>{
+//     res.send('clothing store server is running')
+// })
 
 app.listen(port, ()=>{
     console.log(`clothing store server is running on port ${port}`);
